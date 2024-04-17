5c2459449fdb2f3f34f52ad0dccb24e2c4d69328
ethgraves <ethgraves05@gmail.com>
2024-04-17 17:18:35 -0500

    Television (Version 1.0)

--- television.py
+++ television.py
@@ -14,25 +14,52 @@ class Television:
     #     return self.__status
 
     def power(self):
-        if self.__status == False:
-            self.__status = True
-        else:
+        if self.__status == True:
             self.__status = False
+        else:
+            self.__status = True
 
     def mute(self):
-        pass
+        if self.__status == True:
+            if self.__muted == True:
+                self.__muted = False
+            else:
+                self.__muted = True
 
     def channel_up(self):
-        pass
+        if self.__status == True:
+            if self.__channel == Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
+            else:
+                self.__channel += 1
 
     def channel_down(self):
-        pass
+        if self.__status == True:
+            if self.__channel == Television.MIN_CHANNEL:
+                self.__channel = Television.MAX_CHANNEL
+            else:
+                self.__channel -= 1
 
     def volume_up(self):
-        pass
+        if self.__status == True:
+            if self.__muted == True:
+                self.__muted = False
+            if self.__volume != Television.MAX_VOLUME:
+                self.__volume += 1
 
     def volume_down(self):
-        pass
+        if self.__status == True:
+            if self.__muted == True:
+                self.__muted = False
+            if self.__volume != Television.MIN_VOLUME:
+                self.__volume -= 1
 
     def __str__(self):
-       return f'{self.__status}'
+       return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
+
+
+def main():
+    pass
+
+if __name__ ==  '__main__':
+    main()
