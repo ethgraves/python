9e40beb42c93e424df2eca0f1f83bf3efdf46d5a
ethgraves <ethgraves05@gmail.com>
2024-04-17 22:30:50 -0500

    Television Test (Version 1.1: Started Adding First Tests)

--- test_television.py
+++ test_television.py
@@ -1,1 +1,36 @@
-#filler
+import pytest
+from television import *
+
+class Test:
+    def setup_method(self):
+        self.test1 = Television()
+        self.test2 = Television()
+        self.test3 = Television()
+
+    def teardown_method(self):
+        del self.test1
+        del self.test2
+        del self.test3
+
+    def test_init(self):
+        assert self.test1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    # FIXME: Ask for help on this
+    def test_power(self):
+        self.test1.power()
+        assert self.test1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_mute(self):
+        pass
+
+    def test_channel_up(self):
+        pass
+
+    def test_channel_down(self):
+        pass
+
+    def test_volume_up(self):
+        pass
+
+    def test_volume_down(self):
+        pass
